818e238aa2d2f44fb94cb7c5b90d061a5b98f89f
Brenno Costa <brennovich@riseup.net>
2018-09-10 00:24:52 -0300

    Add custom gaps patch into dwm

--- dwm.h
+++ dwm.h
@@ -3,10 +3,11 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int snap      = 16;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int borderpx  = 4;  /* border pixel of windows */
+static const unsigned int snap      = 16; /* snap pixel */
+static const int showbar            = 1;  /* 0 means no bar */
+static const int topbar             = 1;  /* 0 means bottom bar */
+static const unsigned int gappx     = 6;  /* gap pixel between windows */
 static const char *fonts[]          = { "scientifica:size=9" };
 static const char dmenufont[]       = "scientifica:size=9";
 static const char col_gray1[]       = "#464646";
@@ -64,7 +65,7 @@ static const Layout layouts[] = { /* first entry is default */
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "termite", NULL };
 static const char *brightnessupcmd[]  = { "light", "-A", "10", NULL };
 static const char *brightnessdowncmd[]  = { "light", "-U", "10", NULL };
 
